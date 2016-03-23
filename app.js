46ab74489606dd7952c8796e764294c69a19192b
elisuh <elisaverna@gmail.com>
2016-03-22 21:56:11 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+for (var i = 1; i <= 100; i++) {
+   if (i%15 === 0) {
+     document.write("FIZZBUZZ");
+   }
+   else if (i%3 === 0) {
+    document.write("Fizz");
+  }
+  else if (i%5 === 0) {
+    document.write("Buzz");
+  }
+  else {
+    document.write(i);
+  }
+}
